587b30e7932069a25d375bb134524e67809af04d
Ridwan-Lawal <lawal6382@gmail.com>
2024-01-26 15:22:55 +0100

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+function Button() {
+  return (
+    <button>
+      <section className="">
+        <img src="/icon-html.svg" alt="" />
+      </section>
+    </button>
+  );
+}
+
+export default Button;
